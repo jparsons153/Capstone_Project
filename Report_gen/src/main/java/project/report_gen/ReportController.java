cb6148189458ad946bb961041d9fca8c88666a21
jparsons153 <jparsons153@hotmail.com>
2022-07-19 08:50:39 +0100

    return html endpoint

--- Report_gen/src/main/java/project/report_gen/ReportController.java
+++ Report_gen/src/main/java/project/report_gen/ReportController.java
@@ -2,10 +2,18 @@ package project.report_gen;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ReportController {
 
     // auto wire service method
-    
+
+    @GetMapping("/helloWorld")
+    public String printMessage(){
+        //System.out.println("Hello World from ReportController!");
+
+        return "helloWorld";
+    }
 }
--- Report_gen/src/main/java/project/report_gen/ReportGenApplication.java
+++ Report_gen/src/main/java/project/report_gen/ReportGenApplication.java
@@ -2,15 +2,16 @@ package project.report_gen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ReportGenApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(ReportGenApplication.class, args);
 
-		//System.out.println("Hello World!");
+		System.out.println("Hello World from Bootstrapping class!");
 	}
 
 }
