089850f71f4fbd5489b55ed78df52947c2e34ed3
jparsons153 <jparsons153@hotmail.com>
2022-08-01 18:40:04 +0100

    custom xml part mapping to content control, report

--- Report_gen/src/main/java/project/report_gen/controllers/ReportController.java
+++ Report_gen/src/main/java/project/report_gen/controllers/ReportController.java
@@ -93,7 +93,7 @@ public class ReportController {
 //        boolean USE_SAXON = true; // set this to true; add Saxon to your classpath, and uncomment below
 
 //        String input_DOCX = System.getProperty("user.dir") + "/sample-docs/databinding/invoice.docx";
-		String input_DOCX = "C:/Users/User/OneDrive/Documents/CodingNomads/projects/Capstone_Project/report_gen/src/main/java/project/report_gen/invoice_Saxon_XPath2.docx";
+		String input_DOCX = "C:/Users/User/OneDrive/Documents/CodingNomads/projects/Capstone_Project/report_gen/src/main/java/project/report_gen/JP_Example.docx";
 
         String input_XML = "C:/Users/User/OneDrive/Documents/CodingNomads/projects/Capstone_Project/report_gen/src/main/java/project/report_gen/invoice-data.xml";
 
@@ -148,6 +148,69 @@ public class ReportController {
 		Docx4J.bind(wordMLPackage, xmlDoc, Docx4J.FLAG_BIND_INSERT_XML | Docx4J.FLAG_BIND_BIND_XML,
 				null, new XsltProviderImpl(), null, finisherParams);
 */
+        //Save the document
+        Docx4J.save(wordMLPackage, new File(OUTPUT_DOCX), Docx4J.FLAG_NONE);
+        System.out.println("Saved: " + OUTPUT_DOCX);
+    }
+
+    @GetMapping("/report")
+    @ResponseBody
+    public void bindReportToXML() throws Docx4JException, FileNotFoundException {
+
+        String input_DOCX = "C:/Users/User/OneDrive/Documents/CodingNomads/projects/Capstone_Project/report_gen/src/main/java/project/report_gen/report.docx";
+
+        String input_XML = "C:/Users/User/OneDrive/Documents/CodingNomads/projects/Capstone_Project/report_gen/src/main/java/project/report_gen/report-data.xml";
+
+        // resulting docx
+        String OUTPUT_DOCX = "C:/Users/User/OneDrive/Documents/CodingNomads/projects/Capstone_Project/report_gen/src/main/java/project/report_gen/OUT_ContentControlsMergeXML.docx";
+
+        XPathFactoryUtil.setxPathFactory(new net.sf.saxon.xpath.XPathFactoryImpl());
+
+        // Load input_template.docx
+        WordprocessingMLPackage wordMLPackage = Docx4J.load(new File(input_DOCX));
+
+        // Open the xml stream
+        FileInputStream xmlStream = new FileInputStream(new File(input_XML));
+
+        // Do the binding:
+        // FLAG_NONE means that all the steps of the binding will be done,
+        // otherwise you could pass a combination of the following flags:
+        // FLAG_BIND_INSERT_XML: inject the passed XML into the document
+        // FLAG_BIND_BIND_XML: bind the document and the xml (including any OpenDope handling)
+        // FLAG_BIND_REMOVE_SDT: remove the content controls from the document (only the content remains)
+        // FLAG_BIND_REMOVE_XML: remove the custom xml parts from the document
+
+        // BindingHyperlinkResolver is used by default
+        // BindingHandler.setHyperlinkResolver(new BindingHyperlinkResolverForOpenAPI3());
+        BindingHandler.getHyperlinkResolver().setHyperlinkStyle("Hyperlink");
+
+        // Defaults to ValueInserterPlainTextImpl()
+        // BindingHandler.setValueInserterPlainText(new ValueInserterPlainTextForOpenAPI3());
+
+        //Docx4J.bind(wordMLPackage, xmlStream, Docx4J.FLAG_NONE);
+        //If a document doesn't include the Opendope definitions, eg. the XPathPart,
+        //then the only thing you can do is insert the xml
+        //the example document binding-simple.docx doesn't have an XPathPart....
+        Docx4J.bind(wordMLPackage, xmlStream, Docx4J.FLAG_BIND_INSERT_XML | Docx4J.FLAG_BIND_BIND_XML);// | Docx4J.FLAG_BIND_REMOVE_SDT);
+
+        // That won't invoke any XSLT Finisher (available in 6.1.0).  For that, you need to use signature:
+        //  bind(WordprocessingMLPackage wmlPackage, Document xmlDocument, int flags, DocxFetcher docxFetcher,
+        //  		XsltProvider xsltProvider, String xsltFinisherfilename, Map<String, Map<String, Object>> finisherParams)
+        // Here is an example of using it on invoice.docx to shade a table row:
+/*	    // Signature requires Document, not stream
+  		Document xmlDoc = XmlUtils.getNewDocumentBuilder().parse(xmlStream);
+  		// Specify the finished XSLT we want to us
+	    Docx4jProperties.setProperty("docx4j.model.datastorage.XsltFinisher.xslt",  "XsltFinisherInvoice.xslt");
+	    // Configure properties
+	    Map<String, Map<String, Object>> finisherParams = new HashMap<String, Map<String, Object>>();
+		Map<String, Object> tcPrParams = new HashMap<String, Object>();
+		tcPrParams.put("fillColor", "5250D0"); // color to use for shading
+		finisherParams.put("t_r",  tcPrParams);
+		// Now perform the binding
+		Docx4J.bind(wordMLPackage, xmlDoc, Docx4J.FLAG_BIND_INSERT_XML | Docx4J.FLAG_BIND_BIND_XML,
+				null, new XsltProviderImpl(), null, finisherParams);
+*/
+
         //Save the document
         Docx4J.save(wordMLPackage, new File(OUTPUT_DOCX), Docx4J.FLAG_NONE);
         System.out.println("Saved: " + OUTPUT_DOCX);
