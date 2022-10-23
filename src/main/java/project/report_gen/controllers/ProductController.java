aad7330b25ebe851520856429394bb507e4f6127
jparsons153 <jparsons153@hotmail.com>
2022-10-23 18:39:16 +0100

    added csv file upload

--- src/main/java/project/report_gen/controllers/ProductController.java
+++ src/main/java/project/report_gen/controllers/ProductController.java
@@ -1,13 +1,22 @@
 package project.report_gen.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 import project.report_gen.models.Product;
 import project.report_gen.services.ProductService;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class ProductController {
@@ -23,9 +32,19 @@ public class ProductController {
         return "newProduct";
     }
 
-    @PostMapping("/addProduct")
-    public String saveProduct(@ModelAttribute("product") Product product){
+    @PostMapping(value = "/addProduct",consumes = MediaType.ALL_VALUE)
+    public String saveProduct(@RequestPart("file") MultipartFile file, @ModelAttribute("product") Product product) throws IOException {
         productService.saveProduct(product);
+
+        // pass file to csv method
+
+        String str = new String(file.getBytes(), StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            // Handle the line, ideally in a separate method
+        }
         return "redirect:/new";
     }
 
